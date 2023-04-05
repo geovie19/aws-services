5065eeeb384a305dbc20223eb0ddf65eb6f3bc16
Geovanie I <100096209+geovie19@users.noreply.github.com>
2023-04-04 22:06:28 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  default = "AKIAZ26FJ72GC3NWJQ3Y"
+  default = "AKIAZ26FJ72GA2XPGZLX"
 }
 variable "secret_key" {
-  default = "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"
+  default = "DaFJI48Nwcd4w1nrmqSpMN2C8r+a3/AN47rzVE2U"
 }
 variable "region" {
   default = "us-east-1"
