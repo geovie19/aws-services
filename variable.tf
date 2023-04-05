b9c33ca805baabc4e047c73937c00959a5c56c39
Geovanie I <100096209+geovie19@users.noreply.github.com>
2023-04-04 21:33:56 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "access_key" {
+  default = "AKIAZ26FJ72GC3NWJQ3Y"
+}
+variable "secret_key" {
+  default = "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"
+}
+variable "region" {
+default "us-east-1"
+}
+
