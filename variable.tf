7142043804733149465194096757b2ecd1a76c87
Geovanie I <100096209+geovie19@users.noreply.github.com>
2023-04-04 22:55:47 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,4 @@
-variable "access_key" {
-  default = "AKIAZ26FJ72GA2XPGZLX"
-}
-variable "secret_key" {
-  default = "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"
+
 }
 variable "region" {
   default = "us-east-1"
